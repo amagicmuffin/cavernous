5b4ffe9114b474eff98997335325dda736a19db8
amagicmuffin <62270558+amagicmuffin@users.noreply.github.com>
2020-09-15 09:23:06 -0700

    Update mainIGuess.py

--- mainIGuess.py
+++ mainIGuess.py
@@ -2,14 +2,24 @@ import pygame
 from config import *
 
 
-
-
 crashed = False
 while not crashed:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
+            
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                moveUp()
+            if event.key == pygame.K_a:
+                moveLeft()
+            if event.key == pygame.K_s:
+                moveDown()
+            if event.key == pygame.K_d:
+                moveRight()
+
+
 
         #print(event)
     
@@ -17,3 +27,4 @@ while not crashed:
     
     #pygame.display.update() #only update parts of thing
     #clock.tick(60) this piece of code crashes the thing after three or so seconds :)
+exit()
